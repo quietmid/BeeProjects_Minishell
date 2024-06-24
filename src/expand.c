8f63e04dc26369b7374ecb59f3d8552bcd808ab6
Joseph Lu <jlu@c1r1p3.hive.fi>
2024-06-24 18:53:24 +0300

    new approach, not going to disable quotes until the very end

---
+++ src/expand.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlu <jlu@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/24 13:09:54 by jlu               #+#    #+#             */
+/*   Updated: 2024/06/24 15:43:35 by jlu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*
+will we ever have the siutation were there will be two pairs of double quotes
+void    check_expand(char *input)
+    {
+        int = s_quote;
+        char *str;
+
+        str = input;
+        s_quote = 0;
+        while (*str)
+        {
+            if (*str == 39)
+                s_quote += 1;
+            if (*str == 36 && s_quote % 2 == 0)
+                
+        }
+    }
+*/
